01fdf3522dc17d49045698a52a92a42646070a04
SaharRamezani <saharramezanijolfaie@gmail.com>
2023-01-18 13:32:43 -0500

    q1

---
+++ Function Control Digit.sql
@@ -0,0 +1,41 @@
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Validation]') AND TYPE IN (N'FN', N'IF', N'TF', N'FS', N'FT')) 
+	DROP FUNCTION [dbo].[Validation] 
+GO 
+CREATE FUNCTION [dbo].[Validation](@Str VARCHAR(10)) 
+	RETURNS BIT
+AS 
+BEGIN
+	DECLARE @ValidControlDigit INT,
+			@Counter INT,
+			@IsValid BIT,
+			@ControlDigit INT,
+			@Mod INT
+	SET @IsValid = 0
+	SET @Counter = 2
+	SET @ValidControlDigit = 0
+	SET @ControlDigit = CONVERT(INT, RIGHT(@Str, 1))
+	SET @Str = SUBSTRING(@Str, 1, LEN(@Str) - 1) -- رقم کنترل حذف میشود
+
+	WHILE @Counter <= 10
+	BEGIN
+		SET @ValidControlDigit = @ValidControlDigit + (CONVERT(INT, RIGHT(@Str, 1)) * @Counter)
+		SET @Str = SUBSTRING(@Str, 1, LEN(@Str) - 1) -- یک رقم از سمت راست می اندازیم
+		SET @Counter = @Counter + 1
+	END
+
+	SET @Mod = @ValidControlDigit % 11
+
+	IF @Mod <= 2
+	BEGIN
+		SET @ValidControlDigit = @Mod
+	END
+	ELSE
+	BEGIN
+		SET @ValidControlDigit = 11 - @Mod
+	END
+
+	IF @ValidControlDigit = @ControlDigit
+		SET @IsValid = 1
+
+	RETURN @IsValid
+END
---
+++ Question 1.sql
@@ -0,0 +1,19 @@
+﻿--DROP VIEW NatCode
+
+CREATE VIEW NatCode AS
+
+WITH cte_IsValid AS
+(
+	SELECT CASE WHEN [dbo].[Validation](FORMAT(CONVERT(BIGINT, Customer.NatCod), '0000000000')) = 1 THEN 'Valid' ELSE 'Not Valid' END AS Valid, -- برای اضافه کردن صفر درصورت کم بودن ارقام
+	       Customer.CID
+	FROM Customer
+)
+SELECT Customer.CID,
+       Customer.[Name],
+       Customer.BirthDate,
+	   Customer.[Add],
+	   Customer.Tel,
+	   Customer.NatCod,
+	   cte_IsValid.Valid
+FROM Customer
+INNER JOIN cte_IsValid ON cte_IsValid.CID = Customer.CID
